ad454585de1a184e2cd41e10dff857e585329fdf
devgoldt <kangsun61@gmail.com>
2023-01-29 15:37:25 +0900

    new

--- ch2/printfscanf.c
+++ ch2/printfscanf.c
@@ -75,3 +75,40 @@
 //	printf("%s %s\n", str, a);
 //	return 0;
 //} //문자열 여러개 받기 문제 풀기 통과 22.01.09
+
+int main(void) {
+	
+	char name[256];
+	printf("Name : ");
+	scanf("%s", name, sizeof(name));
+	
+	char id[256];
+	printf("ID : ");
+	scanf("%s", id, sizeof(id));
+
+	int age;
+	printf("Age : ");
+	scanf("%d", &age);
+
+	char birth[256];
+	printf("생일을 입력하세요: (월일, 예: 0802: ");
+	scanf("%s", birth, sizeof(birth));
+
+	int size;
+	printf("Shose size: ");
+	scanf("%d", &size);
+
+	printf("\n\n---User Information---\n\n");
+	printf("name: %s\n", name);
+	printf("ID: %s\n", id);
+	printf("age: %d\n", age);
+	printf("Birth: %s\n", birth);
+	printf("Shoes size: %d\n", size);
+
+	return 0;
+
+
+
+
+
+}
