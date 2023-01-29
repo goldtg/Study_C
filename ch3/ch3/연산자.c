ee788e2f00c2940e6c6b623e3ee09f0b514139af
devgoldt <kangsun61@gmail.com>
2023-01-30 01:54:59 +0900

    23-01-30

---
+++ ch3/ch3/연산자.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+//int main(void) {
+//	int a = 10;
+//	printf("a = %d\n", a);
+//	a = a + 1;
+//	a++;
+//	printf("a = %d\n", a);
+//	a++;
+//	printf("a = %d\n", a);
+//	return 0;
+//
+//}
+
+int main(void) {
+	int b = 20;
+	printf("b = %d\n", ++b);
+	printf("b = %d\n", b++);
+	printf("b = %d\n", b);
+
+
+}
