9baef37a9df3b04cea18775720b8e3e71f4d08a3
devgoldt <kangsun61@gmail.com>
2023-01-30 18:18:54 +0900

    23-01-30

--- ch3/ch3/연산자.c
+++ ch3/ch3/연산자.c
@@ -12,11 +12,39 @@
 //
 //}
 
+//int main(void) {
+//	int b = 20;
+//	printf("b = %d\n", ++b);
+//	printf("b = %d\n", b++);
+//	printf("b = %d\n", b);
+//
+//
+//}
+//예시정리
+
+//int main(void) {
+//	for (int i = 1; i <= 10; i++) {
+//		printf("Hello World %d\n", i);
+//
+//	}
+//	return 0;
+//
+//}
+
+//int main(void) {
+//	int i = 1;
+//	while (i <=10)
+//	{
+//		printf("Hello World %d\n", i++);
+//	}
+//	return 0;
+//} //while문 예시
+
+
 int main(void) {
-	int b = 20;
-	printf("b = %d\n", ++b);
-	printf("b = %d\n", b++);
-	printf("b = %d\n", b);
-
-
+	int i = 1;
+	do {
+		printf("Hello World %d\n", i++);
+	} while (i <= 10);
+	return 0;
 }
