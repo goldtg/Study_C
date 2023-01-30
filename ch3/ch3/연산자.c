259e7349ce1f6ebf5fc0822ce787d8e0942fb4e5
devgoldt <kangsun61@gmail.com>
2023-01-30 18:36:32 +0900

    23-01-30

--- ch3/ch3/연산자.c
+++ ch3/ch3/연산자.c
@@ -41,10 +41,43 @@
 //} //while문 예시
 
 
+//int main(void) {
+//	int i = 1;
+//	do {
+//		printf("Hello World %d\n", i++);
+//	} while (i <= 10);
+//	return 0;
+//}// do while 문 예시
+
+//이중반복문
+
+//int main(void) {
+//	for (int i = 1; i <= 3; i++) {
+//		printf("First Sentense: %d\n", i);
+//		for (int j = 1; j <= 5; j++) {
+//			printf("Second Sentense: %d\n", j);
+//		}
+//
+//	}
+//	return 0;
+//}
+
+//int main(void) {
+//	for (int i = 2; i < 10; i++) {
+//		for (int j = 1; j < 10; j++) {
+//			printf("%d x %d = %d\n", i, j, i * j);
+//		}
+//	}
+//	return 0;
+//} //구구단 만들기
+
 int main(void) {
-	int i = 1;
-	do {
-		printf("Hello World %d\n", i++);
-	} while (i <= 10);
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j <= i; j++) {
+			printf("*");
+		}
+		printf("\n");
+		
+	}
 	return 0;
 }
