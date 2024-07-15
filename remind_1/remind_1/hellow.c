f7e81340f78cfee0332421404fb2562712a08f00
goldtg <kangsun61@gmail.com>
2024-07-15 20:20:47 +0900

    feat: 배열 순서대로 찍기 추가, 합계산 추가

--- remind_1/remind_1/hellow.c
+++ remind_1/remind_1/hellow.c
@@ -407,41 +407,56 @@
 //	}
 //}
 
+//배열 합 계산
+//int main(void) {
+//	int aList[3][4] = {
+//		{ 10, 20, 30 },
+//		{ 40, 50 ,60 }
+//	};
+//	int nMax = 0;
+//
+//
+//	for (int i = 0; i < 2; i++) {
+//		for (int j = 0; j < 3; j++) {
+//			nMax += aList[i][j];
+//		}
+//		aList[i][3] = nMax;
+//	}
+//
+//	for (int i = 0; i < 4; i++) {
+//		int aMax = 0;
+//		for (int j = 0; j < 3; j++) {
+//			aMax += aList[j][i];
+//		}
+//		aList[2][i] = aMax;
+//	}
+//	
+//	//출력
+//	for (int i = 0; i < 3; i++) {
+//		for (int j = 0; j < 4; j++) {
+//			printf("%d\t", aList[i][j]);
+//		}
+//		putchar('\n');
+//	}
+//}
+
 int main(void) {
-	int aList[3][4] = {
-		{ 10, 20, 30 },
-		{ 40, 50 ,60 }
-	};
-	int nMax = 0;
-
-
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 3; j++) {
-			nMax += aList[i][j];
+	int aList[5][5] = { 0 };
+	int aMax = 1;
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			aList[i][j] = aMax;
+			aMax++;
 		}
-		aList[i][3] = nMax;
 	}
-
-	for (int i = 0; i < 4; i++) {
-		int aMax = 0;
-		for (int j = 0; j < 3; j++) {
-			aMax += aList[j][i];
-		}
-		aList[2][i] = aMax;
-	}
-	
-
-
-
-	//출력
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 4; j++) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
 			printf("%d\t", aList[i][j]);
 		}
 		putchar('\n');
 	}
-}
 
+}
 
 
 
