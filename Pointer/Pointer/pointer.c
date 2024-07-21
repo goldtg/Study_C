9a02072cc0399b2326e5a4dce7f826ddd97abfaf
goldtg <kangsun61@gmail.com>
2024-07-21 20:09:15 +0900

    feat: 배열과 포인터 추가

--- Pointer/Pointer/pointer.c
+++ Pointer/Pointer/pointer.c
@@ -16,16 +16,29 @@
 //}
 
 //코드 결함 찾기
-int main(void) {
-	char szBuffer[12] = { "HelloWorld" };
-	char* pszData = NULL;
-	pszData = (char*)malloc(sizeof(char) * 12);
-	//아래 코드는 상수 = 상수를 가르키는 잘못된 예제, 카피를 하고 싶다면 깊은 카피 함수 memcpy를 사용해야함
-	/*pszData = szBuffer;*/
-	pszData = memcpy(pszData, szBuffer, 12);
-	puts(pszData);
+//int main(void) {
+//	char szBuffer[12] = { "HelloWorld" };
+//	char* pszData = NULL;
+//	pszData = (char*)malloc(sizeof(char) * 12);
+//	//아래 코드는 동적 할당된 주소에서 szBuffer 주소로 바뀌면서 동적할당 받은 주소를 유실하게 된다, 카피를 하고 싶다면 깊은 카피 함수 memcpy를 사용해야함
+//	/*pszData = szBuffer;*/
+//	pszData = memcpy(pszData, szBuffer, 12);
+//	puts(pszData);
+//
+//	//메모리 누수 가능성 높음
+//	free(pszData);
+//	return 0;
+//}
 
-	//메모리 누수 가능성 높음
-	free(pszData);
-	return 0;
-}
+//배열 - 포인터 실험
+//int main(void) {
+//	char szBuffer[32] = { "You are a girl." };
+//	printf("%s\n", &szBuffer[4]);
+//	printf("%s\n", &*szBuffer);
+//	printf("%s\n", szBuffer + 4);
+//	printf("%p\n", szBuffer + 4);
+//
+//	return 0;
+//
+//}
+
